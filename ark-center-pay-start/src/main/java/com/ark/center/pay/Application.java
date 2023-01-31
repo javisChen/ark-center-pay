59e17d0dc36e7d1380709a9ed6ce776a9169260c
Javis <javischen9548@foxmail.com>
2023-01-31 18:18:08 +0800

    u

--- ark-center-pay-start/src/main/java/com/ark/center/pay/Application.java
+++ ark-center-pay-start/src/main/java/com/ark/center/pay/Application.java
@@ -1,7 +1,6 @@
 package com.ark.center.pay;
 
 import com.ark.component.web.config.ArkWebConfig;
-import com.ark.component.web.config.CloudAppConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
